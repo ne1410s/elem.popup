f46a5b8cd653199913ca30a21ceacb9dba19ab31
ne1410s <pgj646@gmail.com>
2020-06-25 14:29:49 +0100

    Prettier

--- README.md
+++ README.md
@@ -1,26 +1,34 @@
 # @ne1410s/popup
+
 ## A lightweight & configurable modal popup.
+
 ```html
 <ne14-pop>
   <p>Hello world</p>
 </ne14-pop>
 ```
+
 - **Use:** `<script src="PATH_TO_UMD_SCRIPT"></script>`
-- **Extend:** *npm i @ne1410s/popup*
+- **Extend:** _npm i @ne1410s/popup_
+
 ### Attributes
+
 ```html
-<ne14-pop move resize>YO' STUFF<ne14-pop>
+<ne14-pop move resize>YO' STUFF<ne14-pop></ne14-pop></ne14-pop>
 ```
+
 - **move**: User can drag the popup around
 - **resize**: User can alter the size of the popup
-- **shrink**: (Requires ***resize***: User can reduce size below that initially presented)
+- **shrink**: (Requires **_resize_**: User can reduce size below that initially presented)
 - **open**: The popup is opened immediately
 - **no-close**: The close icon is not made available
 - **style**: If supplied inline, the following styles are propagated to the element. They are fairly self-explanatory
   - `background-color`
   - `border-radius`
   - `box-shadow`
+
 ### Events
+
 ```javascript
 const pop = document.querySelector('ne14-pop');
 
@@ -28,18 +36,23 @@ pop.addEventListener('open', () => {
   console.log('Just opened!');
 });
 ```
+
 - **open**: Fired when the popup is opened
 - **close**: Fired when the popup is closed
 - **confirmaccept**: Fired when the confirm callback allowed the popup to close
 - **confirmreject**: Fired when the confirm callback prevented the popup from closing
 - **dismissaccept**: Fired when the dismiss callback allowed the popup to close
 - **dismissreject**: Fired when the dismiss callback prevented the popup from closing
+
 ### Methods
+
 - **open()**: Opens the popup
 - **close()**: Closes the popup
 - **wrap()**: Wraps existing html element(s) in a new popup
 - **confirm()**: Closes the popup, unless confirmCallback is provided and returns `false`
 - **dismiss()**: Closes the popup, unless dismissCallback is provided and returns `false`.
+
 ### Properties
+
 - **confirmCallback**: () => boolean: Return false to prevent a call to confirm from closing the popup
-- **dismissCallback**: () => boolean: Return false to prevent a call to dismiss from closing the popup 
+- **dismissCallback**: () => boolean: Return false to prevent a call to dismiss from closing the popup
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -4,10 +4,9 @@ import markupUrl from './popup.html';
 import stylesUrl from './popup.css';
 
 export class Popup extends CustomElementBase {
-
   public static readonly observedAttributes = ['open', 'style'];
 
-  private static readonly STYLE_KEYS = [ 
+  private static readonly STYLE_KEYS = [
     'backgroundColor',
     'borderTopLeftRadius',
     'borderTopRightRadius',
@@ -15,12 +14,20 @@ export class Popup extends CustomElementBase {
     'borderBottomLeftRadius',
     'boxShadow',
   ];
-  protected get canMove() { return this.hasAttribute('move'); }
-  protected get canResize() { return this.hasAttribute('resize'); }
-  protected get canShrink() { return this.canResize && this.hasAttribute('shrink'); }
-  private get canClose() { return !this.hasAttribute('no-close'); }
+  protected get canMove() {
+    return this.hasAttribute('move');
+  }
+  protected get canResize() {
+    return this.hasAttribute('resize');
+  }
+  protected get canShrink() {
+    return this.canResize && this.hasAttribute('shrink');
+  }
+  private get canClose() {
+    return !this.hasAttribute('no-close');
+  }
   private _styles: Record<string, string> = {};
-  private _coords: { x: number, y: number };
+  private _coords: { x: number; y: number };
   private _drag: 'back' | 'fore' | 'fore*';
 
   public confirmCallback: () => boolean;
@@ -37,7 +44,7 @@ export class Popup extends CustomElementBase {
     this.resetOffscreenPosition(fore);
 
     closer.addEventListener('click', () => this.close());
-    
+
     resizer.addEventListener('mousedown', () => {
       fore.classList.toggle('over', this.canResize);
       fore.classList.toggle('resizing', this.canResize);
@@ -49,16 +56,15 @@ export class Popup extends CustomElementBase {
       }
     });
 
-    back.addEventListener('mousedown', event => {  
-      this._drag = event.target === back ? 'back' 
-          : event.target === fore ? 'fore'
-          : 'fore*';
-      this._coords = this._drag !== 'fore' ? null 
-          : { x: event.offsetX, y: event.offsetY };
+    back.addEventListener('mousedown', (event) => {
+      this._drag = event.target === back ? 'back' : event.target === fore ? 'fore' : 'fore*';
+      this._coords = this._drag !== 'fore' ? null : { x: event.offsetX, y: event.offsetY };
       fore.classList.toggle('moving', this.canMove && this._drag === 'fore');
     });
-    
-    fore.addEventListener('mouseover', event => fore.classList.toggle('over', this.canMove && event.target === fore));
+
+    fore.addEventListener('mouseover', (event) =>
+      fore.classList.toggle('over', this.canMove && event.target === fore)
+    );
     fore.addEventListener('mouseout', () => fore.classList.remove('over'));
 
     window.addEventListener('mousemove', (event: MouseEvent) => {
@@ -67,13 +73,12 @@ export class Popup extends CustomElementBase {
         const adjust = 5;
         fore.style.width = adjust + event.pageX - rect.x + 'px';
         fore.style.height = adjust + event.pageY - rect.y + 'px';
-      }
-      else if (this.canMove && this._drag === 'fore' && this._coords) {
+      } else if (this.canMove && this._drag === 'fore' && this._coords) {
         const backRect = back.getBoundingClientRect();
         const deltaW = backRect.width - window.innerWidth;
         const deltaH = backRect.height - window.innerHeight;
-        const x_min_pc = 100 * Math.max(0, event.pageX - this._coords.x) / window.innerWidth;
-        const y_min_pc = 100 * Math.max(0, event.pageY - this._coords.y) / window.innerHeight;
+        const x_min_pc = (100 * Math.max(0, event.pageX - this._coords.x)) / window.innerWidth;
+        const y_min_pc = (100 * Math.max(0, event.pageY - this._coords.y)) / window.innerHeight;
         const x_css = `min(${x_min_pc.toFixed(2)}vw, ${deltaW}px + 100vw - 100%)`;
         const y_css = `min(${y_min_pc.toFixed(2)}vh, ${deltaH}px + 100vh - 100%)`;
 
@@ -86,7 +91,7 @@ export class Popup extends CustomElementBase {
       this._drag = null;
       fore.classList.remove('moving', 'resizing');
     });
-    back.addEventListener('mouseup', event => {
+    back.addEventListener('mouseup', (event) => {
       if (this._drag === 'back' && event.target === back) this.dismiss();
       this._drag = null;
       fore.classList.remove('moving', 'resizing');
@@ -96,8 +101,7 @@ export class Popup extends CustomElementBase {
       if (event.propertyName === 'transform') {
         if (back.classList.contains('open')) {
           fore.classList.add('ready');
-        } 
-        else this.resetOffscreenPosition(fore);
+        } else this.resetOffscreenPosition(fore);
       }
     });
   }
@@ -111,9 +115,10 @@ export class Popup extends CustomElementBase {
   }
 
   wrap(selectorOrElem: string | Element): void {
-    const target = typeof (selectorOrElem as Element).append === 'function' 
-      ? selectorOrElem as Element
-      : document.querySelector(selectorOrElem + '');
+    const target =
+      typeof (selectorOrElem as Element).append === 'function'
+        ? (selectorOrElem as Element)
+        : document.querySelector(selectorOrElem + '');
     this.appendChild(target);
   }
 
@@ -138,7 +143,9 @@ export class Popup extends CustomElementBase {
         const fore = back.querySelector('.fore') as HTMLElement;
         const doOpen = !!newValue || typeof newValue === 'string';
         fore.classList.remove('ready', 'moving');
-        fore.style.transform = doOpen ? 'translate(-50%, -50%)' : fore.style.transform + ' translateY(-100vh)';
+        fore.style.transform = doOpen
+          ? 'translate(-50%, -50%)'
+          : fore.style.transform + ' translateY(-100vh)';
         fore.classList.toggle('resize', this.canResize);
         fore.classList.toggle('no-close', !this.canClose);
         back.classList.toggle('open', doOpen);
@@ -151,10 +158,9 @@ export class Popup extends CustomElementBase {
         break;
       case 'style':
         if (newValue) {
-          Popup.STYLE_KEYS
-            .map(key => ({key, value: (this.style as any)[key]}))
-            .filter(kvp => kvp.value)
-            .forEach(kvp => this._styles[kvp.key] = kvp.value);
+          Popup.STYLE_KEYS.map((key) => ({ key, value: (this.style as any)[key] }))
+            .filter((kvp) => kvp.value)
+            .forEach((kvp) => (this._styles[kvp.key] = kvp.value));
           this.removeAttribute('style');
         }
         break;
@@ -177,9 +183,8 @@ export class Popup extends CustomElementBase {
 
   /** Some relevant style properties are propagated */
   private propagateSupportedStyles(backing: HTMLElement) {
-    
     // Backing
-    ['backgroundColor'].forEach(prop => {
+    ['backgroundColor'].forEach((prop) => {
       (backing.style as any)[prop] = this._styles[prop];
     });
 
@@ -191,7 +196,7 @@ export class Popup extends CustomElementBase {
       'borderBottomRightRadius',
       'borderBottomLeftRadius',
       'boxShadow',
-    ].forEach(prop => {
+    ].forEach((prop) => {
       (fore.style as any)[prop] = this._styles[prop];
     });
   }
